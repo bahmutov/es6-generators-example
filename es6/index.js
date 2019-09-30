ec34286f28f9e7fb2763351bca6969c9909c36c9
asadgulxar10 <51773651+asadgulxar10@users.noreply.github.com>
2019-09-30 12:48:50 +0500

    Update index.js

--- es6/index.js
+++ es6/index.js
@@ -1,5 +1,5 @@
 var square = require('./square');
-var squares = square();
+var squares = square(); //it will not call square function instead it will return a generator object
 console.log(squares.next().value);
 console.log(squares.next().value);
 console.log(squares.next().value);
